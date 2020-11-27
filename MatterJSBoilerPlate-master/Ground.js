ea4aa058be5b2366b727937b931a72e1a4668178
ompatil2745 <73051105+ompatil2745@users.noreply.github.com>
2020-11-27 13:48:25 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/Ground.js
@@ -0,0 +1,34 @@
+class Ground{
+
+    constructor (x,y,w,h){
+    
+    var options={
+    
+    isStatic:true
+  
+    
+    }
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
+    this.body=Bodies.circle(x,y,w,h,options);
+    World.add(world,this.body);
+    
+    
+    
+    }    
+    display(){
+    
+    var groundpos=this.body.position;
+    push();
+    translate(groundpos.x,groundpos.y);
+    rectMode(CENTER);
+    //strokeWeight(4);
+    fill(128,128,128);
+    rect(0,0,this.w,this.h);
+    pop();
+    
+    }
+    
+    }
